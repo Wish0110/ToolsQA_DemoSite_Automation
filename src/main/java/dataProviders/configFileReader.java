4c8fb6ff7b0ed7b4ad6d36d65ecae433af765545
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2025-04-12 20:12:21 +0530

    Data Providers configFileReader

---
+++ src/main/java/dataProviders/configFileReader.java
@@ -0,0 +1,8 @@
+package dataProviders;
+
+public class configFileReader {
+
+    public static final String locatorRepositoryPath = "data/locatorsRepository.xlsx";
+    public static final String locatorWorksheet = "Web";
+
+}
